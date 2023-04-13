f028b4d9f4fab556595abac40b3375ade1d1b3bd
Serkan Varışlı <87394796+serkanvarisli@users.noreply.github.com>
2023-04-13 20:42:26 +0300

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/header";
+import Footer from "./components/footer";
 import Container from "./components/container";
-import Darkmode from "./components/Darkmode";
+import Darkmode from "./components/darkmode";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import React from "react";
+import { ReactDOM } from "react";
 
 function App() {
   return (
